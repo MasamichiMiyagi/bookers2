001a124688c1923842a8a96b769b030beb87484d
MasamichiMiyagi <114850152+MasamichiMiyagi@users.noreply.github.com>
2022-10-22 22:22:48 +0000

    [2022/10/23/7:22]bookers2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,15 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
-    user_path(resource)
+    if @user
+      redirect_to user_path(resource), notice: 'Signed in successfully.'
+    else
+      render user_session_path
+    end
   end
 
   def after_sign_out_path_for(resource)
-    root_path
+    redirect_to root_path, notice: 'Signed out successfully.'
   end
 
   protected
